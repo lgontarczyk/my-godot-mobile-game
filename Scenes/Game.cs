ead44c8a57ed363b7088c2997061dbbb4eedbd4e
lgontarczyk <39104102+lgontarczyk@users.noreply.github.com>
2025-05-02 23:32:46 +0200

    Assets & empty scenes

---
+++ Scenes/Game.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+namespace MyMobileGame.Scenes;
+
+public partial class Game : Node2D
+{
+	public override void _Ready()
+	{
+	}
+	
+	public override void _Process(double delta)
+	{
+	}
+}
